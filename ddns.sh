c3c9498dc05ba4207f44a969601950ff3a92579d
John Torres <jjtcsuf@gmail.com>
2023-05-05 11:22:12 -0700

    fix: sleep for 30s to make sure networking is ready

--- ddns.sh
+++ ddns.sh
@@ -2,7 +2,7 @@
 
 # Integrates with the No-IP API to fetch the WAN IP address of this host and
 # update the dynamic DNS entry.
-
+sleep 30
 wan=$(curl -s http://ip1.dynupdate.no-ip.com/)
 hostname=${ddns_hostname}
 url=http://dynupdate.no-ip.com/nic/update?hostname=$hostname&myip=$wan 

db7dce218eacfdc3b408d4a94360a4f201f10eb5
John Torres <jjtcsuf@gmail.com>
2023-05-05 15:24:20 -0700

    fix: simplify variables

--- ddns.sh
+++ ddns.sh
@@ -2,11 +2,10 @@
 
 # Integrates with the No-IP API to fetch the WAN IP address of this host and
 # update the dynamic DNS entry.
-sleep 30
+
 wan=$(curl -s http://ip1.dynupdate.no-ip.com/)
 hostname=${ddns_hostname}
-url=http://dynupdate.no-ip.com/nic/update?hostname=$hostname&myip=$wan 
-username=${ddns_username}
-password=${ddns_password}
+url="http://dynupdate.no-ip.com/nic/update?hostname=$hostname&myip=$wan"
+auth="${ddns_username}:${ddns_password}"
 
-curl -s --user $username:$password $url
+curl -s --user $auth $url
